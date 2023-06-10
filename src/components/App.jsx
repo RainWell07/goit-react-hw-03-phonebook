9917c291a24a2a2936106324281101d522db0632
RainWell07 <117939087+RainWell07@users.noreply.github.com>
2023-06-10 11:43:25 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,24 +6,21 @@ import { nanoid } from "nanoid";
 import css from "../Modules/phoneBook.module.css";
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      contacts: [],
-      filter: '',
-    };
-  }
+  state = {
+    contacts: [],
+    filter: '',
+  };
 
   componentDidMount() {
     const localContacts = localStorage.getItem('contacts');
     if (localContacts !== null) {
-      this.setState({contacts: JSON.parse(localContacts)})
+      this.setState({ contacts: JSON.parse(localContacts) });
     }
   }
 
-  componentDidUpdate(prevState) {
-    const {contacts} = this.state;
-    const  prevContacts = prevState.contacts;
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    const prevContacts = prevState.contacts;
     if (contacts !== prevContacts) {
       localStorage.setItem('contacts', JSON.stringify(contacts));
     }
@@ -42,32 +39,32 @@ class App extends Component {
     }));
   };
 
-    deleteContact = (id) => {
-      this.setState((prevState) => ({
-        contacts: prevState.contacts.filter((contact) => contact.id !== id),
-      }));
-    };
+  deleteContact = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== id),
+    }));
+  };
 
-    handleFilterChange = (value) => {
-      this.setState({filter: value});
-    };
+  handleFilterChange = (value) => {
+    this.setState({ filter: value });
+  };
 
-    render() {
-      const { contacts, filter} = this.state;
-      const filteredContacts = contacts.filter((contact) =>
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = contacts.filter((contact) =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
-      );
+    );
 
-      return (
-        <div className={`${css.container} ${css.basicFont}`}>
+    return (
+      <div className={`${css.container} ${css.basicFont}`}>
         <h1 className={`${css.basicFont} ${css.logo}`}>Phonebook</h1>
         <ContactForm addContact={this.addContact} />
         <h2 className={`${css.basicFont} ${css.logo}`}>Contacts</h2>
         <Filter filter={filter} handleFilterChange={this.handleFilterChange} />
         <ContactList contacts={filteredContacts} deleteContact={this.deleteContact} />
       </div>
-      );
-}
+    );
+  }
 }
 
 export default App;
